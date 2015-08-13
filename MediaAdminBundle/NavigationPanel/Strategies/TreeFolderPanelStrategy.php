a5231ef048d26093f042df8b297f1ea2ee5d6d59
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-08-13 11:36:49 +0200

    add weight in navigation strategy

--- MediaAdminBundle/NavigationPanel/Strategies/TreeFolderPanelStrategy.php
+++ MediaAdminBundle/NavigationPanel/Strategies/TreeFolderPanelStrategy.php
@@ -63,6 +63,14 @@ class TreeFolderPanelStrategy extends AbstractNavigationPanelStrategy
     }
 
     /**
+     * @return int
+     */
+    public function getWeight()
+    {
+        return 6;
+    }
+
+    /**
      * @return string
      */
     public function getRole()
