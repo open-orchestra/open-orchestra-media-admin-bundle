3f896485729a6af1c8383b65bddc932f190666ca
itkg-ngilain <noel.gilain@businessdecision.com>
2015-08-24 17:11:21 +0200

    Update the symlink grunt task option to allow it to declare multiple symlinks

---
+++ GruntTasks/Options/symlink.js
@@ -0,0 +1,6 @@
+module.exports = {
+    no_image: {
+        src: 'web/bundles/openorchestramediaadmin/images/no_image_available.jpg',
+        dest: 'web/img/no_image_available.jpg'
+    }
+};
--- GruntTasks/Options/symlink.no_image.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    src: 'web/bundles/openorchestramediaadmin/images/no_image_available.jpg',
-    dest: 'web/img/no_image_available.jpg'
-};
