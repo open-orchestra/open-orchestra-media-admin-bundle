210c634c9f14616e834f908d4ae3e1db2427806e
itkg-ngilain <noel.gilain@businessdecision.com>
2015-10-20 18:31:56 +0200

    Make the folders in media modal closable

--- GruntTasks/Options/concat.mediacss.js
+++ GruntTasks/Options/concat.mediacss.js
@@ -1,7 +1,8 @@
 module.exports = {
     src: [
         'web/built/openorchestramediaadmin/css/media.css',
-        'web/built/openorchestramediaadmin/css/mediaModal.css'
+        'web/built/openorchestramediaadmin/css/mediaModal.css',
+        'web/built/openorchestramediaadmin/css/mediaModalFolder.css'
     ],
     dest: 'web/built/media.css'
 };
