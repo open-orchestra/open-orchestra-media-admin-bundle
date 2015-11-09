e7519241e5cd990864370da3b0144e0ec4afdc1f
itkg-ngilain <noel.gilain@businessdecision.com>
2015-11-09 16:46:53 +0100

    Update media upload using file chunks (flow-php-server)

--- MediaAdminBundle/Controller/Api/MediaController.php
+++ MediaAdminBundle/Controller/Api/MediaController.php
@@ -12,6 +12,7 @@ use OpenOrchestra\MediaAdminBundle\Exceptions\HttpException\MediaNotDeletableExc
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Flow\Basic as FlowBasic;
 
 /**
  * Class MediaController
@@ -98,25 +99,63 @@ class MediaController extends BaseController
      * @Config\Route("/upload/{folderId}", name="open_orchestra_api_media_upload")
      * Config\Method({"POST"})
      * 
-     * @return Response
+     * @return FacadeInterface
      */
     public function uploadAction($folderId, Request $request)
     {
         $uploadedFile = $request->files->get('file');
 
         if ($uploadedFile) {
-            $folderRepository = $this->get('open_orchestra_media.repository.media_folder');
-            $folder = $folderRepository->find($folderId);
+            $tmpDir = $this->getParameter('open_orchestra_media.tmp_dir');
+            $fileName = $this->generateTmpName($uploadedFile);
 
-            $mediaClass = $this->container->getParameter('open_orchestra_media.document.media.class');
-            $media = new $mediaClass();
-            $media->setMediaFolder($folder);
-            $media->setFile($uploadedFile);
+            if (FlowBasic::save($tmpDir . '/' . $fileName, $tmpDir)) {
+                $media = $this->createMedia($uploadedFile, $fileName);
 
-            $documentManager = $this->get('object_manager');
-            $documentManager->persist($media);
+                return $this->get('open_orchestra_api.transformer_manager')->get('media')->transform($media);
+            }
         }
 
         return array();
+    }
+
+    /**
+     * Generate the tmp file name used to glue chunk files
+     * 
+     * @param Symfony\Component\HttpFoundation\File\UploadedFile $uploadedFile
+     * 
+     * @return string
+     */
+    protected function generateTmpName($uploadedFile)
+    {
+        $tmpDir = $this->getParameter('open_orchestra_media.tmp_dir');
+
+        return sha1(uniqid(mt_rand(), true))
+            . pathinfo($tmpDir . '/' . $uploadedFile->getClientOriginalName(), PATHINFO_FILENAME)
+            . '.' . $uploadedFile->guessClientExtension();
+    }
+
+    /**
+     * @param Symfony\Component\HttpFoundation\File\UploadedFile $uploadedFile
+     * @param string                                             $fileName
+     * 
+     * @return OpenOrchestra\Media\Model\MediaInterface
+     */
+    protected function createMedia($uploadedFile, $fileName)
+    {
+        $folderRepository = $this->get('open_orchestra_media.repository.media_folder');
+        $folder = $folderRepository->find($folderId);
+
+        $mediaClass = $this->container->getParameter('open_orchestra_media.document.media.class');
+        $media = new $mediaClass();
+        $media->setMediaFolder($folder);
+        $media->setFile($uploadedFile);
+        $media->setFilesystemName($fileName);
+
+        $documentManager = $this->get('object_manager');
+        $documentManager->persist($media);
+        $documentManager->flush();
+
+        return $media;
     }
 }
