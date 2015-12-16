caa1b57a6f9ffec64c63d2d7720cf7885c3f340c
soleng <sola-prog@live.fr>
2015-12-16 10:58:09 +0100

    fix height overflow on preview and no resizing for the incorrect image aspect
    fix height overflow on preview
    
    reformat for visibility
    
    ...
    
    **
    
    no resizing for the incorrect image aspect

--- MediaAdmin/FileUtils/Image/ImagickImageManager.php
+++ MediaAdmin/FileUtils/Image/ImagickImageManager.php
@@ -25,7 +25,7 @@ class ImagickImageManager implements ImageManagerInterface
     /**
      * @param string $filePath
      * @param array  $format
-     * 
+     *
      * @return string
      */
     public function generateAlternative($filePath, array $format)
@@ -45,7 +45,7 @@ class ImagickImageManager implements ImageManagerInterface
      *
      * @param array   $format
      * @param Imagick $image
-     * 
+     *
      * @return Imagick
      */
     protected function resizeImage(array $format, Imagick $image)
@@ -58,9 +58,9 @@ class ImagickImageManager implements ImageManagerInterface
             $refRatio = $maxWidth / $maxHeight;
             $imageRatio = $image->getImageWidth() / $image->getImageHeight();
 
-            if ($refRatio > $imageRatio || $maxWidth == -1) {
+            if ($imageRatio >= 1 / $maxHeight && ($refRatio > $imageRatio || $maxWidth == -1)) {
                 $image = $this->resizeOnHeight($image, $maxHeight);
-            } else {
+            } else if ($imageRatio <= 1 / $maxWidth) {
                 $image = $this->resizeOnWidth($image, $maxWidth);
             }
         }
@@ -70,10 +70,10 @@ class ImagickImageManager implements ImageManagerInterface
 
     /**
      * Resize an image keeping its ratio to the height $height
-     * 
+     *
      * @param Imagick $image
      * @param int     $height
-     * 
+     *
      * @return Imagick
      */
     protected function resizeOnHeight(Imagick $image, $height)
@@ -85,10 +85,10 @@ class ImagickImageManager implements ImageManagerInterface
 
     /**
      * Resize an image keeping its ratio to the width $width
-     * 
+     *
      * @param Imagick $image
      * @param int     $width
-     * 
+     *
      * @return Imagick
      */
     protected function resizeOnWidth(Imagick $image, $width)
@@ -100,11 +100,11 @@ class ImagickImageManager implements ImageManagerInterface
 
     /**
      * Compress and save $image
-     * 
+     *
      * @param string  $filePath
      * @param Imagick $image
      * @param int     $compression_quality
-     * 
+     *
      * @return Imagick
      */
     protected function saveImage($filePath, Imagick $image, $compression_quality)
@@ -119,10 +119,10 @@ class ImagickImageManager implements ImageManagerInterface
 
     /**
      * Extract an image from the $page of $filePath
-     * 
+     *
      * @param string $filePath
      * @param int    $page
-     * 
+     *
      * @return string
      */
     public function extractImageFromPdf($filePath, $page = 0)
@@ -140,14 +140,14 @@ class ImagickImageManager implements ImageManagerInterface
 
     /**
      * Crop $filePath with ($x, $y, $h, $w) and resize it to the $formatName
-     * 
+     *
      * @param string $filePath
      * @param int    $x
      * @param int    $y
      * @param int    $h
      * @param int    $w
      * @param array  $format
-     * 
+     *
      * @return string
      */
     public function cropAndResize($filePath, $x, $y, $h, $w, array $format)
