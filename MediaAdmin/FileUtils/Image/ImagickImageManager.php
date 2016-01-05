d2aa1c9ee9ee05f1776576fe315405f12dd0a24e
soleng <sola-prog@live.fr>
2016-01-05 12:45:28 +0100

    add new rezise method by liquid rescale

--- MediaAdmin/FileUtils/Image/ImagickImageManager.php
+++ MediaAdmin/FileUtils/Image/ImagickImageManager.php
@@ -50,17 +50,27 @@ class ImagickImageManager implements ImageManagerInterface
      */
     protected function resizeImage(array $format, Imagick $image)
     {
-        $maxWidth = array_key_exists('max_width', $format)? $format['max_width']: -1;
-        $maxHeight = array_key_exists('max_height', $format)? $format['max_height']: -1;
+        $maxWidth = array_key_exists('max_width', $format) ? $format['max_width']: -1;
+        $maxHeight = array_key_exists('max_height', $format) ? $format['max_height']: -1;
 
         if (-2 != $maxWidth + $maxHeight) {
             $image->setimagebackgroundcolor('#000000');
             $refRatio = $maxWidth / $maxHeight;
-            $imageRatio = $image->getImageWidth() / $image->getImageHeight();
-            if ($imageRatio >= 1 / $maxHeight && ($refRatio > $imageRatio || $maxWidth == -1)) {
-                $image = $this->resizeOnHeight($image, $maxHeight);
-            } else if ($imageRatio >= 1 / $maxWidth) {
-                $image = $this->resizeOnWidth($image, $maxWidth);
+            $imageRatioByHeight = $image->getImageWidth() / $image->getImageHeight();
+            $imageRatioByWidth = $image->getImageHeight() / $image->getImageWidth();
+
+            if ($refRatio > $imageRatioByHeight || $maxWidth == -1) {
+                if ($imageRatioByHeight >= 1 / $maxHeight) {
+                    $image = $this->resizeOnHeight($image, $maxHeight);
+                } else {
+                    $image = $this->liquidRescaleImageOnHeight($image, $maxHeight);
+                }
+            } else {
+                if ($imageRatioByWidth >= 1 / $maxWidth) {
+                    $image = $this->resizeOnWidth($image, $maxWidth);
+                } else {
+                    $image = $this->liquidRescaleImageOnWidth($image,$maxWidth);
+                }
             }
         }
 
@@ -93,6 +103,36 @@ class ImagickImageManager implements ImageManagerInterface
     protected function resizeOnWidth(Imagick $image, $width)
     {
         $image->resizeImage($width, 0, Imagick::FILTER_LANCZOS, 1);
+
+        return $image;
+    }
+
+    /**
+     * Scales the images using liquid rescaling method to height $height
+     *
+     * @param Imagick $image
+     * @param int     $height
+     *
+     * @return Imagick
+     */
+    protected function liquidRescaleImageOnHeight(Imagick $image, $height)
+    {
+        $image->liquidRescaleImage($image->getImageWidth(), $height, 0, 0);
+
+        return $image;
+    }
+
+    /**
+     * Scales the images using liquid rescaling method to width $width
+     *
+     * @param Imagick $image
+     * @param int     $width
+     *
+     * @return Imagick
+     */
+    protected function liquidRescaleImageOnWidth(Imagick $image, $width)
+    {
+        $image->liquidRescaleImage($width, $image->getImageHeight(), 0, 0);
 
         return $image;
     }
