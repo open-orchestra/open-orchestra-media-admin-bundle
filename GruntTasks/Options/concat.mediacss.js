3c6efb7c717a6423162cd609f5e2d59df657996b
itkg-ngilain <noel.gilain@businessdecision.com>
2016-01-15 16:59:48 +0100

    Rename Options folder into Targets

--- GruntTasks/Options/concat.mediacss.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    src: [
-        'web/built/openorchestramediaadmin/css/media.css',
-        'web/built/openorchestramediaadmin/css/mediaModal.css',
-        'web/built/openorchestramediaadmin/css/mediaModalFolder.css',
-        'web/built/openorchestramediaadmin/css/mediaUpload.css'
-    ],
-    dest: 'web/built/media.css'
-};
