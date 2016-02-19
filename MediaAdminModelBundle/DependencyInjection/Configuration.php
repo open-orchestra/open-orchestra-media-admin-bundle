f560b7745b7840600695fa75dea18fb2c28b2cbc
Thibault Lefrançois <thi.lefrancois@gmail.com>
2016-02-19 17:39:17 +0100

    Create MediaAdminModelBundle

---
+++ MediaAdminModelBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OpenOrchestra\MediaAdminModelBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $treeBuilder->root('open_orchestra_media_admin_model');
+
+        return $treeBuilder;
+    }
+}
