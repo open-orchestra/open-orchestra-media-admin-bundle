923ad04277ac3bd537edfd257b0cf1a88f53f820
Ali Oumar <benomar01@gmail.com>
2016-04-05 15:05:43 +0200

    remove unused js file

--- GruntTasks/Targets/concat.media_js.js
+++ GruntTasks/Targets/concat.media_js.js
@@ -3,8 +3,6 @@ module.exports = {
     'vendor/bower_components/flow.js/dist/flow.js',
     'web/built/openorchestramediaadmin/js/groupTree/*.js',
     'web/built/openorchestramediaadmin/js/tinyMCE/*.js',
-    //--[ BUTTON RIBBON ]--//
-    'web/built/openorchestramediaadmin/js/widgetToolbarButton/widgetToolbarButton.js',
     'web/built/openorchestramediaadmin/js/view/extendView/*.js',
     'web/built/openorchestramediaadmin/js/view/*.js',
     'web/built/openorchestramediaadmin/js/media/*.js',
