4539fbc65180ab35844c06deed5f58c34c806ef1
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-05-25 16:04:23 +0200

    refresh test

--- MediaAdminBundle/Tests/Functional/Repository/MediaRepositoryTest.php
+++ MediaAdminBundle/Tests/Functional/Repository/MediaRepositoryTest.php
@@ -16,6 +16,7 @@ class MediaRepositoryTest extends AbstractKernelTestCase
      * @var MediaRepositoryInterface
      */
     protected $repository;
+    protected $keywordsLabelToId;
 
     /**
      * Set up test
@@ -25,6 +26,12 @@ class MediaRepositoryTest extends AbstractKernelTestCase
         parent::setUp();
 
         static::bootKernel();
+        $keywordRepository = static::$kernel->getContainer()->get('open_orchestra_model.repository.keyword');
+        $keywords = $keywordRepository->findAll();
+        $this->keywordsLabelToId = array();
+        foreach($keywords as $keywords) {
+            $this->keywordsLabelToId[$keywords->getLabel()] = $keywords->getId();
+        }
         $this->repository = static::$kernel->getContainer()->get('open_orchestra_media.repository.media');
     }
 
@@ -36,6 +43,7 @@ class MediaRepositoryTest extends AbstractKernelTestCase
      */
     public function testFindByKeywords($keywords, $count)
     {
+        $keywords = implode(',', $this->replaceKeywordLabelById(explode(',', $keywords)));
         $keywords = $this->repository->findByKeywords($keywords);
 
         $this->assertCount($count, $keywords);
@@ -47,10 +55,24 @@ class MediaRepositoryTest extends AbstractKernelTestCase
     public function provideKeywordAndCount()
     {
         return array(
-            array('Lorem', 5),
-            array('Sit', 0),
-            array('Dolor', 4),
-            array('Lorem,Dolor', 5),
+            array('lorem', 5),
+            array('sit', 0),
+            array('dolor', 4),
+            array('lorem,dolor', 5),
         );
+    }
+
+    /**
+     * @param array $data
+     */
+    protected function replaceKeywordLabelById($data)
+    {
+        $keywordsLabelToId = $this->keywordsLabelToId;
+        array_walk_recursive($data, function (&$item, $key) use ($keywordsLabelToId) {
+            if (is_string($item)) {
+                $item = str_replace(array_keys($keywordsLabelToId), $keywordsLabelToId, $item);
+            }
+        });
+        return $data;
     }
 }
