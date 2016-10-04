fba01e149e9728ad67631f46585121293db4f45b
itkg-ngilain <noel.gilain@businessdecision.com>
2016-10-04 11:45:50 +0200

    Filter groups on Mongo side rather than on php side when creating a folder

--- MediaAdminModelBundle/EventListener/AddMediaFolderGroupRoleForFolderListener.php
+++ MediaAdminModelBundle/EventListener/AddMediaFolderGroupRoleForFolderListener.php
@@ -18,28 +18,25 @@ class AddMediaFolderGroupRoleForFolderListener extends AbstractMediaFolderGroupR
     {
         $document = $event->getDocument();
         if ($document instanceof FolderInterface) {
+            $site = $this->container->get('open_orchestra_model.repository.site')->findOneBySiteId($document->getSiteId());
+            $groups = $this->container->get('open_orchestra_user.repository.group')->findAllWithSiteId($site->getId());
             $accessType = $this->getFolderAccessType($document);
-            $siteId = $document->getSiteId();
-            $groups = $this->container->get('open_orchestra_user.repository.group')->findAllWithSite();
             $mediaFolderRoles = $this->getMediaFolderRoles();
+
             /** @var GroupInterface $group */
             $groupsToFlush = array();
 
             foreach ($groups as $group) {
-
-                if ($group->getSite()->getSiteId() == $siteId) {
-
-                    foreach ($mediaFolderRoles as $role => $translation) {
-                        if (false === $group->hasModelGroupRoleByTypeAndIdAndRole(FolderInterface::GROUP_ROLE_TYPE, $document->getId(), $role)) {
-                            $mediaFolderRole = $this->createMediaFolderGroupRole($document, $group, $role, $accessType);
-                            $group->addModelGroupRole($mediaFolderRole);
-                            $groupsToFlush[$group->getId()] = $group;
-                        }
+                foreach ($mediaFolderRoles as $role => $translation) {
+                    if (false === $group->hasModelGroupRoleByTypeAndIdAndRole(FolderInterface::GROUP_ROLE_TYPE, $document->getId(), $role)) {
+                        $mediaFolderRole = $this->createMediaFolderGroupRole($document, $group, $role, $accessType);
+                        $group->addModelGroupRole($mediaFolderRole);
+                        $groupsToFlush[$group->getId()] = $group;
                     }
+                }
 
-                    if (isset($groupsToFlush[$group->getId()])) {
-                        $event->getDocumentManager()->persist($group);
-                    }
+                if (isset($groupsToFlush[$group->getId()])) {
+                    $event->getDocumentManager()->persist($group);
                 }
             }
 
