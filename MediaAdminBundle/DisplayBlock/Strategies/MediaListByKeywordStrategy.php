93e7ba63c1085ada609dbe269ae902b75f2b6f92
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-11-15 14:49:20 +0100

    fix getclass

--- MediaAdminBundle/DisplayBlock/Strategies/MediaListByKeywordStrategy.php
+++ MediaAdminBundle/DisplayBlock/Strategies/MediaListByKeywordStrategy.php
@@ -37,7 +37,7 @@ class MediaListByKeywordStrategy extends AbstractStrategy
             'OpenOrchestraMediaAdminBundle:Block/MediaList:show.html.twig',
             array(
                 'id' => $block->getId(),
-                'class' => $block->getClass(),
+                'class' => $block->getStyle(),
                 'keywords' => $block->getAttribute('keywords')
             )
         );
