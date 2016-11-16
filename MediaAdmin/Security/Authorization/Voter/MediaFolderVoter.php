362905cb20cfb34b3cd0fdb672bb7396ee92fe00
itkg-ngilain <noel.gilain@businessdecision.com>
2016-11-16 16:28:51 +0100

    Refacto AbstractPerimeterVoter

--- MediaAdmin/Security/Authorization/Voter/MediaFolderVoter.php
+++ MediaAdmin/Security/Authorization/Voter/MediaFolderVoter.php
@@ -2,7 +2,6 @@
 
 namespace OpenOrchestra\MediaAdmin\Security\Authorization\Voter;
 
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use OpenOrchestra\Media\Model\MediaFolderInterface;
 use OpenOrchestra\UserBundle\Model\UserInterface;
 use OpenOrchestra\Backoffice\Security\ContributionActionInterface;
@@ -24,42 +23,16 @@ class MediaFolderVoter extends AbstractPerimeterVoter
     }
 
     /**
-     * @return array
-     */
-    protected function getSupportedAttributes()
-    {
-        return array(
-            ContributionActionInterface::READ,
-            ContributionActionInterface::ADD,
-            ContributionActionInterface::EDIT,
-            ContributionActionInterface::DELETE
-        );
-    }
-
-    /**
-     * @param string         $attribute
-     * @param mixed          $subject
-     * @param TokenInterface $token
+     * Vote for Read action
+     *
+     * @param MediaFolderInterface $folder
+     * @param UserInterface        $user
      *
      * @return bool
      */
-    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    protected function voteForReadAction($folder, $user)
     {
-        $user = $token->getUser();
-
-        if ($this->isSuperAdmin($user)) {
-            return true;
-        }
-
-        if (ContributionActionInterface::READ == $attribute) {
-            return $this->isSubjectInAllowedPerimeter($subject->getPath(), $user, MediaFolderInterface::ENTITY_TYPE);
-        }
-
-        if ($subject->getCreatedBy() == $user->getUsername()) {
-            return $this->voteForOwnedMediaFolder($attribute, $subject, $user);
-        }
-
-        return $this->voteForSomeoneElseMediaFolder($attribute, $subject, $user);
+        return $this->isSubjectInAllowedPerimeter($subject->getPath(), $user, MediaFolderInterface::ENTITY_TYPE);
     }
 
     /**
@@ -71,7 +44,7 @@ class MediaFolderVoter extends AbstractPerimeterVoter
      *
      * @return bool
      */
-    protected function voteForOwnedMediaFolder($action, MediaFolderInterface $folder, UserInterface $user)
+    protected function voteForOwnedSubject($action, $folder, UserInterface $user)
     {
         return $user->hasRole(ContributionRoleInterface::MEDIA_FOLDER_CONTRIBUTOR)
             && $this->isSubjectInAllowedPerimeter($folder->getPath(), $user, MediaFolderInterface::ENTITY_TYPE);
@@ -86,17 +59,17 @@ class MediaFolderVoter extends AbstractPerimeterVoter
      *
      * @return bool
      */
-    protected function voteForSomeoneElseMediaFolder($action, MediaFolderInterface $folder, UserInterface $user)
+    protected function voteForSomeoneElseSubject($action, $folder, UserInterface $user)
     {
         $requiredRole = ContributionRoleInterface::MEDIA_FOLDER_CONTRIBUTOR;
 
         switch ($action) {
             case ContributionActionInterface::EDIT:
                 $requiredRole = ContributionRoleInterface::MEDIA_FOLDER_SUPER_EDITOR;
-                break;
+            break;
             case ContributionActionInterface::DELETE:
                 $requiredRole = ContributionRoleInterface::MEDIA_FOLDER_SUPER_SUPRESSOR;
-                break;
+            break;
         }
 
         return $user->hasRole($requiredRole)
