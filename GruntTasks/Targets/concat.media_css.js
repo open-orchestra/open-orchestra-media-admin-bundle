f70cf9354ac00ac48529913597ccd0e1eebc86ad
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-02-28 10:45:03 +0100

    remove useless grunt task

--- GruntTasks/Targets/concat.media_css.js
+++ GruntTasks/Targets/concat.media_css.js
@@ -1,11 +1,6 @@
 module.exports = {
   src: [
-    'web/built/openorchestramediaadmin/css/media.css',
-    'web/built/openorchestramediaadmin/css/mediaModal.css',
-    'web/built/openorchestramediaadmin/css/mediaModalFolder.css',
-    'web/built/openorchestramediaadmin/css/mediaUpload.css',
-    'web/built/openorchestramediaadmin/css/groupFolderTreePanel.css',
-    'web/built/openorchestramediaadmin/css/form.css'
+    'web/built/openorchestramediaadmin/css/media.css'
   ],
   dest: 'web/built/media.css'
 };
