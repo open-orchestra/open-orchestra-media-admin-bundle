f1960652d1e6a15027b60f601fc70427cbc54a87
itkg-nanne <nicolas.anne@businessdecision.com>
2017-03-17 09:57:12 +0100

    fix name

--- MediaAdminBundle/Resources/public/ecmascript/OpenOrchestra/Application/Router/AbstractMediaRouter.js
+++ MediaAdminBundle/Resources/public/ecmascript/OpenOrchestra/Application/Router/AbstractMediaRouter.js
@@ -14,7 +14,7 @@ class AbstractMediaRouter extends OrchestraRouter
                 label:Translator.trans('open_orchestra_backoffice.menu.contribution.title')
             },
             {
-                label: Translator.trans('open_orchestra_media_admin.menus.contribution.media')
+                label: Translator.trans('open_orchestra_media_admin.menu.contribution.media')
             },
             [
                 {
