d4970840412fb3c9d500c10d26589b9f4426790f
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-03-22 09:17:43 +0100

    remove uselessUse (#365)

--- MediaAdminBundle/Controller/Admin/FolderController.php
+++ MediaAdminBundle/Controller/Admin/FolderController.php
@@ -1,13 +1,14 @@
 <?php
+
 namespace OpenOrchestra\MediaAdminBundle\Controller\Admin;
+
 use OpenOrchestra\BackofficeBundle\Controller\AbstractAdminController;
 use OpenOrchestra\MediaAdmin\FolderEvents;
-use OpenOrchestra\Media\Model\FolderInterface;
-use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
 use Symfony\Component\HttpFoundation\Response;
 use OpenOrchestra\Backoffice\Security\ContributionActionInterface;
+
 /**
  * Class FolderController
  */
