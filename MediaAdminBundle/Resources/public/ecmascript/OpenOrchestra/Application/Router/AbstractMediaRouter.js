f47f321ff2ec172abd335ef0280096fd48ee4e5f
itkg-ngilain <noel.gilain@businessdecision.com>
2017-03-22 11:33:06 +0100

    Fix media sub-menu access

--- MediaAdminBundle/Resources/public/ecmascript/OpenOrchestra/Application/Router/AbstractMediaRouter.js
+++ MediaAdminBundle/Resources/public/ecmascript/OpenOrchestra/Application/Router/AbstractMediaRouter.js
@@ -1,4 +1,5 @@
 import OrchestraRouter from './OrchestraRouter'
+import Application     from '../../Application/Application'
 
 /**
  * @class AbstractMediaRouter
@@ -9,6 +10,22 @@ class AbstractMediaRouter extends OrchestraRouter
      * @inheritdoc
      */
     getBreadcrumb() {
+        var subMenuItem = [];
+        if (Application.getContext().user.access_section.media) {
+            subMenuItem.push({
+                label: Translator.trans('open_orchestra_media_admin.media.title_list'),
+                link : '#'+Backbone.history.generateUrl('listMedia'),
+                id   : 'navigation-media'
+            });
+        }
+        if (Application.getContext().user.access_section.media_folder) {
+            subMenuItem.push({
+                label: Translator.trans('open_orchestra_media_admin.folder.title_list'),
+                link : '#'+Backbone.history.generateUrl('listFolders'),
+                id   : 'navigation-folder'
+            });
+        }
+
         return [
             {
                 label:Translator.trans('open_orchestra_backoffice.menu.contribution.title')
@@ -16,19 +33,8 @@ class AbstractMediaRouter extends OrchestraRouter
             {
                 label: Translator.trans('open_orchestra_media_admin.menu.contribution.media')
             },
-            [
-                {
-                    label: Translator.trans('open_orchestra_media_admin.media.title_list'),
-                    link: '#'+Backbone.history.generateUrl('listMedia'),
-                    id : 'navigation-media'
-                },
-                {
-                    label: Translator.trans('open_orchestra_media_admin.folder.title_list'),
-                    link: '#'+Backbone.history.generateUrl('listFolders'),
-                    id : 'navigation-folder'
-                }
-            ]
-        ]
+            subMenuItem
+         ]
     }
 }
 
