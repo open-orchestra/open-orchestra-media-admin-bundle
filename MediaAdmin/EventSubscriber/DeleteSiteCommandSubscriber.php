420da693ad920f1488128469cd12a319c1c4f5d0
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-04-14 14:32:01 +0200

    extract

--- MediaAdmin/EventSubscriber/DeleteSiteCommandSubscriber.php
+++ MediaAdmin/EventSubscriber/DeleteSiteCommandSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace OpenOrchestra\MediaAdmin\EventSubscriber;
 
+use OpenOrchestra\Backoffice\Command\OrchestraDeleteSiteTools;
 use OpenOrchestra\Backoffice\Event\SiteCommandEvent;
 use OpenOrchestra\Backoffice\SiteCommandEvents;
+use OpenOrchestra\Media\Repository\FolderRepositoryInterface;
 use OpenOrchestra\Media\Repository\MediaRepositoryInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -13,14 +15,26 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class DeleteSiteCommandSubscriber implements EventSubscriberInterface
 {
     protected $mediaRepository;
+    protected $folderRepository;
+    protected $deleteSiteTools;
+    protected $mediaClass;
 
     /**
-     * DeleteSiteCommandSubscriber constructor.
-     * @param MediaRepositoryInterface $mediaRepository
+     * @param MediaRepositoryInterface  $mediaRepository
+     * @param FolderRepositoryInterface $folderRepository
+     * @param OrchestraDeleteSiteTools  $deleteSiteTools
+     * @param string                    $mediaClass
      */
-    public function __construct(MediaRepositoryInterface $mediaRepository)
-    {
+    public function __construct(
+        MediaRepositoryInterface $mediaRepository,
+        FolderRepositoryInterface $folderRepository,
+        OrchestraDeleteSiteTools $deleteSiteTools,
+        $mediaClass
+    ) {
         $this->mediaRepository = $mediaRepository;
+        $this->folderRepository = $folderRepository;
+        $this->deleteSiteTools = $deleteSiteTools;
+        $this->mediaClass = $mediaClass;
     }
 
     /**
@@ -29,9 +43,33 @@ class DeleteSiteCommandSubscriber implements EventSubscriberInterface
     public function checkHardDeleteSite(SiteCommandEvent $siteEvent)
     {
         $siteId = $siteEvent->getSite()->getSiteId();
-        $medias = $this->mediaRepository->findWithUseReferences($siteId);
+        $io = $siteEvent->getIo();
 
-        throw  new \RuntimeException('test');
+        $medias = $this->mediaRepository->findWithUseReferences($siteId);
+        $usageMedias = $this->deleteSiteTools->findUsageReferenceInOtherSite($siteId, $medias);
+        if (!empty($usageMedias)) {
+            $io->section('Usage of media in other sites');
+            $this->deleteSiteTools->displayUsedReferences($io, $usageMedias);
+            throw new \RuntimeException('You should remove usage of nodes before remove site '.$siteId);
+        }
+    }
+
+    /**
+     * @param SiteCommandEvent $siteEvent
+     */
+    public function deleteMedia(SiteCommandEvent $siteEvent)
+    {
+        $siteId = $siteEvent->getSite()->getSiteId();
+        $io = $siteEvent->getIo();
+
+        $io->comment('Remove use references of medias');
+        $this->deleteSiteTools->removeUseReferenceEntity($siteId, $this->mediaClass);
+
+        $io->comment('Remove medias');
+        $this->mediaRepository->removeAllBySiteId($siteId);
+
+        $io->comment('Remove folders');
+        $this->folderRepository->removeAllBySiteId($siteId);
     }
 
     /**
@@ -41,6 +79,7 @@ class DeleteSiteCommandSubscriber implements EventSubscriberInterface
     {
         return array(
             SiteCommandEvents::SITE_CHECK_HARD_DELETE => 'checkHardDeleteSite',
+            SiteCommandEvents::SITE_HARD_DELETE => 'deleteMedia',
         );
     }
 }
