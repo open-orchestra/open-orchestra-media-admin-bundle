94059fa5d1bb72963117cef9c52c468de8e6bf2a
itkg-nanne <nicolas.anne@businessdecision.com>
2017-05-15 09:57:50 +0200

    change selector fo order

--- MediaAdminBundle/Resources/public/ecmascript/OpenOrchestra/Application/View/Media/MediaListView.js
+++ MediaAdminBundle/Resources/public/ecmascript/OpenOrchestra/Application/View/Media/MediaListView.js
@@ -13,7 +13,7 @@ class MediaListView extends mix(AbstractDataTableView).with(UrlPaginateViewMixin
     preinitialize(options) {
         super.preinitialize(options);
         this.events = this.events || {};
-        this.events['click .order-value'] = '_changeOrder';
+        this.events['click a.order-value'] = '_changeOrder';
     }
 
     /**
