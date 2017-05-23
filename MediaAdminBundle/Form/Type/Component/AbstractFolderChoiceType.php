4eaffdf5ea3aa2105bb5cceee789485f40aba714
itkg-nanne <nicolas.anne@businessdecision.com>
2017-05-23 09:07:44 +0200

    move form type

---
+++ MediaAdminBundle/Form/Type/Component/AbstractFolderChoiceType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OpenOrchestra\MediaAdminBundle\Form\Type\Component;
+
+use Symfony\Component\Form\AbstractType;
+
+/**
+ * Class AbstractFolderChoiceType
+ */
+abstract class AbstractFolderChoiceType extends AbstractType
+{
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'oo_folder_choice';
+    }
+}
